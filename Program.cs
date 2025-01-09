ab022ffefdc77b174c567d1ff082476480a940d5
ItamarLit <itamarlitvin@gmail.com>
2025-01-09 10:53:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OmegaSudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
