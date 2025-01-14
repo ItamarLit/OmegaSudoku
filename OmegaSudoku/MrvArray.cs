18bc13ab1907cdd8b801c6df98cf5b4f00dca2df
ItamarLit <itamarlitvin@gmail.com>
2025-01-14 21:56:11 +0200

    Started work on MrvArray

---
+++ OmegaSudoku/MrvArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OmegaSudoku
+{
+    class MrvArray
+    {
+        /// <summary>
+        /// This class holds the mrv array that is used to get the cell with the minimum amount of possibilites in it
+        /// </summary>
+
+        // Create an Arra
+        private Dictionary<(int, int), BoardCell>[] MRVPossibilitesArray { get ; set; }
+
+        public MrvArray(int boardSize)
+        {
+            // Create the array of hashmaps where a (x,y) tuple is the key and a BoardCell is the value
+            MRVPossibilitesArray = new Dictionary<(int, int), BoardCell>[boardSize + 1];
+            // Init the hashmaps inside the array, cell 1 will represent cells with only one possibility and so on
+            for (int i = 1; i < boardSize + 1; i++)
+            {
+                MRVPossibilitesArray[i] = new Dictionary<(int, int), BoardCell>();
+            }
+        }
+
+        public BoardCell RemoveCell(int possibilitesNum, (int x, int y) tuple)
+        {
+            // Func that removes a cell from the MrvArray
+            return null;
+        }
+
+        public void InsertCell(int possibilitesNum, (int x, int y) tuple, BoardCell cell)
+        {
+            // Func that inserts a cell into the MrvArray
+        }
+
+    }
+}
