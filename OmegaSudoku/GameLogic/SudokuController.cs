3da0a310ef133d336bdab369bfaad693698385a4
ItamarLit <itamarlitvin@gmail.com>
2025-01-16 16:58:42 +0200

    Added board setup func

--- OmegaSudoku/GameLogic/SudokuController.cs
+++ OmegaSudoku/GameLogic/SudokuController.cs
@@ -12,13 +12,68 @@ namespace OmegaSudoku.GameLogic
         /// and it will also include the function that will attempt to solve the board
         /// </summary>
         public BoardCell[,] GameBoard { get; }
+        public MrvArray MrvArray { get; }
 
         public SudokuController(BoardCell[,] board)
         {
             GameBoard = board;
+            // create the mrv array
+            MrvArray = new MrvArray(GameBoard.GetLength(0));
         }
 
-        public List<(int, int)> GetRowCells(int rowLvl)
+        public void CheckInitalBoard()
+        {
+            // go over all the filled cells and check that they are valid
+            foreach ((int cellRow, int cellCol) in GetAllBoardCells())
+            {
+                // check if the cell in the cube has the same value of the checked cell
+                if (GetCellValue(cellRow, cellCol) != 0 && !IsValidMove(cellRow, cellCol))
+                {
+                    // NEED to throw an exception
+                    Console.WriteLine($"Bad init board at pos: {cellRow}, {cellCol}");
+                }
+            }
+            Console.WriteLine("Valid board");
+        }
+
+        public void SetInitailBoardPossibilites()
+        {
+            // set the board possibilites
+            foreach ((int cellX, int cellY) in GetAllBoardCells())
+            {
+                // check if the cell in the cube has the same value of the checked cell
+                if (GetCellValue(cellX, cellY) != 0)
+                {
+                    DecreasePossibilites(cellX, cellY, GetCellValue(cellX, cellY) );
+                }
+            }
+            // after setting the board up we can mrv array
+            foreach ((int cellX, int cellY) in GetAllBoardCells())
+            {
+                if (GetCellValue(cellX, cellY) == 0)
+                {
+                    // insert the cell into the array
+                    MrvArray.InsertCell(GameBoard[cellX, cellY]);
+                }
+            }
+        }
+
+
+        private List<(int, int)> GetAllBoardCells()
+        {
+            // this func will return a list of tuples of row, col positions of all cells on the board
+            List<(int, int)> boardCells = new List<(int, int)>();
+            for (int row = 0; row < GameBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < GameBoard.GetLength(1); col++) 
+                {
+                    boardCells.Add((row, col));
+                }
+            }
+            return boardCells;
+        }
+
+        private List<(int, int)> GetRowCells(int rowLvl)
         {
             // this func will return a list of tuples of row, col positions inside a row
             List<(int, int)> rowCells = new List<(int, int)>();
@@ -29,7 +84,7 @@ namespace OmegaSudoku.GameLogic
             return rowCells;
         }
 
-        public List<(int, int)> GetColumnCells(int columnNum)
+        private List<(int, int)> GetColumnCells(int columnNum)
         {
             // this func will return a list of tuples of row, col positions inside a column
             List<(int, int)> columnCells = new List<(int, int)>();
@@ -40,7 +95,7 @@ namespace OmegaSudoku.GameLogic
             return columnCells; 
         }
 
-        public List<(int, int)> GetCubeCells(int rowPos, int colPos)
+        private List<(int, int)> GetCubeCells(int rowPos, int colPos)
         {
             // this func will return a list of tuples of row, col positons inside a cube
             // cube size is root of the board width / length
@@ -61,20 +116,20 @@ namespace OmegaSudoku.GameLogic
 
         }
 
-        public int GetCellValue(int rowPos, int colPos)
+        private int GetCellValue(int rowPos, int colPos)
         {
             // get the game cell value
             return GameBoard[rowPos, colPos].CellValue;
         }
 
-        public bool IsValidMove(int rowPos, int colPos)
+        private bool IsValidMove(int rowPos, int colPos)
         {
             // this func will return true / false if the move is valid
             int cellValue = GetCellValue(rowPos, colPos);   
             return IsValidCol(colPos, cellValue) && IsValidRow(rowPos, cellValue) && IsValidCube(rowPos, colPos, cellValue);
         }
 
-        public bool IsValidRow(int rowLvl, int cellValue)
+        private bool IsValidRow(int rowLvl, int cellValue)
         {
             // func that returns if a row is valid
             List<(int, int)> rowCells = GetRowCells(rowLvl);
@@ -82,21 +137,21 @@ namespace OmegaSudoku.GameLogic
 
         }
 
-        public bool IsValidCol(int colNum, int cellValue)
+        private bool IsValidCol(int colNum, int cellValue)
         {
             // func that returns if a col is valid
             List<(int, int)> colCells = GetColumnCells(colNum);
             return CheckForDoubles(colCells, cellValue);
         }
 
-        public bool IsValidCube(int rowPos, int colPos, int cellValue)
+        private bool IsValidCube(int rowPos, int colPos, int cellValue)
         {
             // func that returns if a cube is valid
             List<(int, int)> cubeCells = GetCubeCells(rowPos, colPos);
             return CheckForDoubles(cubeCells, cellValue);
         }
 
-        public bool CheckForDoubles(List<(int, int)> cells, int cellValue)
+        private bool CheckForDoubles(List<(int, int)> cells, int cellValue)
         {
             // this func is used to check for illegal double cell values in a cell list
             int counter = 0;
@@ -110,6 +165,28 @@ namespace OmegaSudoku.GameLogic
                 }
             }
             return counter == 1;
-        } 
+        }
+
+        private void DecreasePossibilites(int rowPos, int colPos, int valueToRemove)
+        {
+            // This func is used to reduce the board possibilites based on the current change
+            // create an array of lists of cell positons to remove
+            List<(int, int)>[] unitCellsArray = { GetRowCells(rowPos), GetColumnCells(colPos), GetCubeCells(rowPos, colPos) };
+            // the number of rowCells = colCells = cubeCells
+            int cellCount = unitCellsArray[0].Count;
+
+            for (int i = 0; i < unitCellsArray.Length; i++)
+            {
+                for (int innerIndex = 0; innerIndex < cellCount; innerIndex++) 
+                {
+                    int cellRow = unitCellsArray[i][innerIndex].Item1;
+                    int cellCol = unitCellsArray[i][innerIndex].Item2;
+                    // attempt to remove the value
+                    Console.WriteLine($"{cellRow}, {cellCol}");
+                    GameBoard[cellRow, cellCol].DecreasePossibility(valueToRemove);
+                }
+            }
+        }
+
     }
 }
