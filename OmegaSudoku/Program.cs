9a67694817bf46927820666b4d52cba8a7c922a4
ItamarLit <itamarlitvin@gmail.com>
2025-01-17 21:29:50 +0200

    Added main sudoku engine code

--- OmegaSudoku/Program.cs
+++ OmegaSudoku/Program.cs
@@ -1,24 +1,16 @@
 ﻿using OmegaSudoku.GameLogic;
 using OmegaSudoku.IO;
 using System;
+
+
 namespace OmegaSudoku
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            OutputHandler.RequestBoard();
-            InputHandler.GetUserInput();
-            InputHandler.CheckInput();
-            BoardCell[,] board = InputHandler.SetUpBoard();
-            OutputHandler.PrintBoard(board);
-            MrvArray mrvArray = new MrvArray(board.GetLength(0));
-            SudokuSolver ss = new SudokuSolver(board, mrvArray);
-            OutputHandler.PrintBoard(board);
-            //OutputHandler.PrintBoard(board);
-            
-            //mrvArray.PrintArray();
+            // run the engine
+            SudokuEngine.RunEngine();
         }
     }
 }
---
+++ OmegaSudoku/SudokuEngine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OmegaSudoku;
+using OmegaSudoku.Exceptions;
+using OmegaSudoku.GameLogic;
+using OmegaSudoku.IO;
+
+namespace OmegaSudoku
+{
+    class SudokuEngine
+    {
+        public static void RunEngine()
+        {
+            bool endRunFlag = false;
+            OutputHandler.ShowInstructions();
+            while (!endRunFlag) 
+            {
+                try
+                {
+                    OutputHandler.RequestBoard();
+                    InputHandler.GetUserInput();
+                    if (!InputHandler.CheckInput())
+                    {
+                        endRunFlag = true;
+                    }
+                    else
+                    {
+                        BoardCell[,] board = InputHandler.SetUpBoard();
+                        OutputHandler.PrintBoard(board);
+                        MrvArray mrvArray = new MrvArray(board.GetLength(0));
+                        SudokuSolver solver = new SudokuSolver(board, mrvArray);
+                        if (solver.Solve())
+                        {
+                            OutputHandler.PrintBoard(board);
+                        }
+                        else
+                        {
+                            OutputHandler.ShowImpossibleBoardMsg();
+                        }
+                    }
+                }
+                // The only excptions that can occur are mine so i can catch Exception
+                catch (Exception e) 
+                {
+                    OutputHandler.OutputError(e.Message);
+                }
+               
+            }
+        }
+    }
+}
