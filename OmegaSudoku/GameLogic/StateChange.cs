e8f8a7297e3800507a0e8979c202f34015180aad
ItamarLit <itamarlitvin@gmail.com>
2025-01-22 20:41:40 +0200

    Added struct for stack usage

---
+++ OmegaSudoku/GameLogic/StateChange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku.GameLogic
+{
+    public struct StateChange
+    {
+        /// <summary>
+        /// This struct is used to hold data of changes during backtracking, 
+        /// it has a hash set of cellValue changes and a hash set of cellPossibility changes
+        /// </summary>
+        public HashSet<(int row, int col, int oldValue)> CellValueChanges;
+        public HashSet<(int row, int col, int removedValue)> CellPossibilityChanges;
+
+        public StateChange()
+        {
+            CellValueChanges = new HashSet<(int row, int col, int oldValue)>();
+            CellPossibilityChanges = new HashSet<(int row, int col, int removedValue)>();
+        }
+    }
+}
