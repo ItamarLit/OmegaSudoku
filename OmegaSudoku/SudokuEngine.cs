2eeaac7f0161512c21f1b56e407fcf876b870f06
ItamarLit <itamarlitvin@gmail.com>
2025-01-27 19:08:38 +0200

    Refactoring engine

--- OmegaSudoku/SudokuEngine.cs
+++ OmegaSudoku/SudokuEngine.cs
@@ -4,54 +4,112 @@ using OmegaSudoku.Exceptions;
 using OmegaSudoku.GameLogic.Heurisitcs;
 using OmegaSudoku.GameLogic;
 using OmegaSudoku.IO;
+using OmegaSudoku.Interfaces;
 
 namespace OmegaSudoku
 {
     class SudokuEngine
     {
+        /// <summary>
+        /// This is the engine class, it runs the solver
+        /// </summary>
+        /// 
         public static void RunEngine()
         {
+            // main engine func, used to get the user input and choices
             bool endRunFlag = false;
-            Console.WriteLine("Hi welcome to the amazing sudoku engine, N * N board, to exit the engine write: EXIT");
-            while (!endRunFlag) 
+            Console.WriteLine("*** Welcome to the Amazing Sudoku Solver ***\n");
+            OutputHandler.ShowMenu();
+            ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+            while (!endRunFlag)
             {
-                //try
-                //{
-                    SudokuSolver.depth = 0;
-                    OutputHandler.RequestBoard();
-                    InputHandler.GetUserInput();
-                    if (!InputHandler.CheckInput())
-                    {
+                Console.Write("\n\nEnter your choice: ");
+                consoleInputHandler.GetUserInput();
+                string choice = consoleInputHandler.Input.Trim().ToLower();
+                switch (choice)
+                {
+                    case "solve_c":
+                        SolveBoardFromConsole(consoleInputHandler);
+                        break;
+                    case "solve_f":
+                        SolveBoardFromFile(consoleInputHandler);
+                        break;
+                    case "menu":
+                        OutputHandler.ShowMenu();
+                        break;
+                    case "rules":
+                        OutputHandler.ShowRules();
+                        break;
+                    case "exit":
+                        Console.WriteLine("Exiting the solver!");
                         endRunFlag = true;
-                    }
-                    else
-                    {
-                        Icell[,] board = InputHandler.SetUpBoard();
-                        OutputHandler.PrintBoard(board);
-                        Mrvdict mrvDict = new Mrvdict(board.GetLength(0));
-                        SudokuSolver solver = new SudokuSolver(board, mrvDict);
-                        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-                        if (solver.Solve())
-                        {
-                            OutputHandler.PrintBoard(board);
-                        }
-                        else
-                        {
-                            OutputHandler.ShowImpossibleBoardMsg();
-
-                        }
-                        stopwatch.Stop();
-                        OutputHandler.ShowProgramRuntime(stopwatch.ElapsedMilliseconds);
-                        Console.WriteLine($"Depth: {SudokuSolver.depth}");
-                    }
-                //}
-                //catch (Exception e)
-                //{
-                //    //The only excptions that can occur are mine so i can catch Exception
-                //    OutputHandler.OutputError(e.Message);
-                //}
-
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid choice: {choice} entered.");
+                        break;
+                }
             }
+        }
+
+        private static void SolveBoardFromConsole(ConsoleInputHandler consoleInputHandler)
+        {
+            // func to solve boards from console
+            try
+            {
+                Console.WriteLine("Enter the Sudoku board:");
+                consoleInputHandler.GetUserInput();
+                string input = consoleInputHandler.Input.Trim();
+                SolveBoard(input);
+            }
+            catch (Exception e)
+            {
+                OutputHandler.OutputError(e.Message);
+            }
+        }
+
+        private static void SolveBoardFromFile(ConsoleInputHandler consoleInputHandler)
+        {
+            // func to solve boards from file
+            try
+            {
+                Console.Write("Enter the file path of the Sudoku board: ");
+                consoleInputHandler.GetUserInput();
+                // get the file path
+                string path = consoleInputHandler.Input.Trim();
+                FileInputHandler fileInputHandler = new FileInputHandler(path);
+                fileInputHandler.GetUserInput();
+                string input = fileInputHandler.Input.Trim();
+                SolveBoard(input);
+            }
+            catch (Exception e)
+            {
+                OutputHandler.OutputError(e.Message);
+            }
+        }
+
+        private static void SolveBoard(string input)
+        {
+            SudokuSolver.depth = 0;
+            // check the input
+            InputValidator.CheckInput(input);
+            Icell[,] board = InputValidator.SetUpBoard(input);
+            OutputHandler.PrintBoard(board);
+            // set up the data structs
+            Mrvdict mrvDict = new Mrvdict(board.GetLength(0));
+            // set up solver
+            SudokuSolver solver = new SudokuSolver(board, mrvDict);
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            if (solver.Solve())
+            {
+                OutputHandler.PrintBoard(board);
+            }
+            else
+            {
+                OutputHandler.ShowImpossibleBoardMsg();
+            }
+            stopwatch.Stop();
+            OutputHandler.ShowProgramRuntime(stopwatch.ElapsedMilliseconds);
+            Console.WriteLine($"Depth: {SudokuSolver.depth}");
         }
     }
 }
